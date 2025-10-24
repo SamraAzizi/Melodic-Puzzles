f71d809e3720b196127618f719d29d178ef24c2f
SamraAzizi <samarraazizi@gmail.com>
2025-10-24 18:24:48 +0500

    melodic puzzles game

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { GameBoard } from "@/components/GameBoard";
+
+const Index = () => {
+  return (
+    <div className="min-h-screen bg-gradient-background flex items-center justify-center p-4">
+      <GameBoard />
+    </div>
+  );
+};
+
+export default Index;
