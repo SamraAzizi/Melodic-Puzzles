b79260d8133646ce001507fb32b5064cafdc7b3b
SamraAzizi <samarraazizi@gmail.com>
2025-10-25 20:54:46 +0500

    melodic puzzles game

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -66,3 +66,20 @@ const addToRemoveQueue = (toastId: string) => {
 
   toastTimeouts.set(toastId, timeout);
 };
+
+export const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case "ADD_TOAST":
+      return {
+        ...state,
+        toasts: [action.toast, ...state.toasts].slice(0, TOAST_LIMIT),
+      };
+
+    case "UPDATE_TOAST":
+      return {
+        ...state,
+        toasts: state.toasts.map((t) => (t.id === action.toast.id ? { ...t, ...action.toast } : t)),
+      };
+
+    case "DISMISS_TOAST": {
+      const { toastId } = action;
