588d7f5e2875b0599f295be443effedbcd0359cb
SamraAzizi <samarraazizi@gmail.com>
2025-10-25 20:53:19 +0500

    melodic puzzles game

--- src/hooks/use-toast.ts
+++ src/hooks/use-toast.ts
@@ -15,3 +15,20 @@ type ToasterToast = ToastProps & {
 const actionTypes = {
   ADD_TOAST: "ADD_TOAST",
   UPDATE_TOAST: "UPDATE_TOAST",
+  DISMISS_TOAST: "DISMISS_TOAST",
+  REMOVE_TOAST: "REMOVE_TOAST",
+} as const;
+
+let count = 0;
+
+function genId() {
+  count = (count + 1) % Number.MAX_SAFE_INTEGER;
+  return count.toString();
+}
+
+type ActionType = typeof actionTypes;
+
+type Action =
+  | {
+      type: ActionType["ADD_TOAST"];
+      toast: ToasterToast;
