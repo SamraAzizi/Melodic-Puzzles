66f83ae497f63323d6163411a30b2f46fd33cbde
SamraAzizi <samarraazizi@gmail.com>
2025-10-31 21:07:18 +0500

    melodic puzzles game

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -14,3 +14,16 @@ const badgeVariants = cva(
         outline: "text-foreground",
       },
     },
+    defaultVariants: {
+      variant: "default",
+    },
+  },
+);
+
+export interface BadgeProps extends React.HTMLAttributes<HTMLDivElement>, VariantProps<typeof badgeVariants> {}
+
+function Badge({ className, variant, ...props }: BadgeProps) {
+  return <div className={cn(badgeVariants({ variant }), className)} {...props} />;
+}
+
+export { Badge, badgeVariants };
